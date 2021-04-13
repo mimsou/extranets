7e52ad598aec10e51c7b4ac811f1f4ff675ddfc0
Jean-Christophe Gaudette <jcgaudette@mediasimple.ca>
2021-04-13 15:50:01 -0400

    Mise à jour placeholder - Regroupement

--- resources/views/admin/candidats/partials/_informations.blade.php
+++ resources/views/admin/candidats/partials/_informations.blade.php
@@ -17,7 +17,7 @@
                     <div class="col-md-6">
                         <div class="form-group">
                             <label for="regroupement_id">{{ __('Regroupement') }} *</label>
-                            {{ Form::select('regroupement_id', \App\Models\Regroupement::pluck('title', 'id'), null, ['required', 'class'=>'form-control', 'id'=>'nom', 'placeholder'=>"S'il y a lieu"]) }}
+                            {{ Form::select('regroupement_id', \App\Models\Regroupement::pluck('title', 'id'), null, ['required', 'class'=>'form-control', 'id'=>'nom', 'placeholder'=>"Aucun"]) }}
                         </div>
                     </div>
                 </div>
