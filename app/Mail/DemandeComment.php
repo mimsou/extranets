685968346b3035a66ad4d0f52f4eb29cbcad7503
Michael <micbhatti@gmail.com>
2021-05-13 00:53:24 +0530

    Mail QA fix

--- app/Mail/DemandeComment.php
+++ app/Mail/DemandeComment.php
@@ -28,6 +28,6 @@ class DemandeComment extends Mailable
      */
     public function build()
     {
-        return $this->view('email.demande.commented');
+        return $this->markdown('email.demande.commented');
     }
 }
