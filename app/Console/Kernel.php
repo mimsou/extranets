236c6669702ec9dc647c54180464ae1633865012
Michael Mike <micbhatti@gmail.com>
2021-09-09 12:05:19 -0500

    fixed delete chunks command issue

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('chunks:clear')->weeklyOn(0, '23:50');
+//        $schedule->command('chunks:clear')->weeklyOn(0, '23:50');
+        $schedule->command('chunks:clear')->weekly()->at('23:50');
 
     }
 
